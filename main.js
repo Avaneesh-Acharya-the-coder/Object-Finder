53352b2d88216559f0c5e89a9dd5a53aaeb74785
Avaneesh-Acharya-the-coder <145211628+Avaneesh-Acharya-the-coder@users.noreply.github.com>
2023-09-23 11:28:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+video=""
+flag=""
+function preload() {
+    video=createVideo("video.mp4")
+    video.hide()
+}
 function setup() {
-    canvas=createCanvas(1000, 600)
+    canvas=createCanvas(400, 300)
+    canvas.center()
+}
+function draw() {
+    image(video, 0, 0, 400, 300)
+}
+function start() {
+    obj_det=ml5.objectDetector("cocossd", model_loaded)
+    document.getElementById("status").innerHTML="status: Detecting Objects..."
+}
+function model_loaded() {
+    console.log("model is LOADED.")
+    flag=true
+    video.loop()
+    video.speed(1.25)
+    video.volume(0.0)
 }
